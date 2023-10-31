70f646faa83cf6dcd4143e6bbc8bad7d162fda26
Leo from DT-6084 <rovingwind93@gmail.com>
2023-10-31 11:39:43 +0800

    update

---
+++ Routine/Nuget-Server/Nuget_Delete.ps1
@@ -0,0 +1,19 @@
+﻿# Nuget-Server 刪除套件 版本
+
+$Version = "2.0.0"
+$PackageName = "Utility"
+
+$ConfigPath = "$PSScriptRoot\..\..\config.json"
+$ConfigExist = Test-Path -Path $ConfigPath -PathType Leaf
+
+Try {
+	if (!$ConfigExist) { throw "$ConfigPath 設定檔不存在"	}
+	$Config = Get-Content -Path $ConfigPath | ConvertFrom-Json
+	
+	dotnet nuget delete $PackageName $Version -s $Config.Baget.Url -k $Config.Baget.Key
+} Catch {
+	Write-Host $_.Exception.Message -ForegroundColor Red
+	Write-Host $_.ScriptStackTrace
+}
+
+Pause
---
+++ Routine/Nuget-Server/Nuget_Push.ps1
@@ -0,0 +1,19 @@
+﻿# Nuget-Server 發行套件
+
+$Version = "2.0.0"
+
+$ConfigPath = "$PSScriptRoot\..\..\config.json"
+$ConfigExist = Test-Path -Path $ConfigPath -PathType Leaf
+
+Try {
+	if (!$ConfigExist) { throw "$ConfigPath 設定檔不存在"	}
+	$Config = Get-Content -Path $ConfigPath | ConvertFrom-Json
+	
+	$Path = "$($Config.Baget.PackagePath)\$($Config.Baget.PackageName).${Version}.nupkg"
+	dotnet nuget push $Path -k $Config.Baget.Key -s $Config.Baget.Url
+} Catch {
+	Write-Host $_.Exception.Message -ForegroundColor Red
+	Write-Host $_.ScriptStackTrace
+}
+
+Pause
