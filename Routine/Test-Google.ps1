397abdbce9310a1c6f8731a76de8431ba121fe86
Leo from DT-6084 <rovingwind93@gmail.com>
2023-12-25 18:03:29 +0800

    update

--- Routine/Test-Google.ps1
+++ Routine/Test-Google.ps1
@@ -7,30 +7,58 @@ $LibraryPath = $Env:PS_Library
 $ConfigPath = $Env:PS_Config
 
 # 自訂變數
-$Time = 5
+## 回應時間閾值(ms)
+$ResponseTimeLimit = 4
+## 目標 Domain
 $Url = "www.google.com"
+## 測試時間間隔(s)
+$Intervals = 5
 
 function Run {
-	$IsRun = $true
-	# 持續 ping www.google.com
-	while ($IsRun) {
-		if($IsPowerShell7) {
-			$pingResult = Test-Connection -ComputerName $Url -Count 1
-			$responseTime = $pingResult.Latency
-		} else {
+	# 持續 Test 目標
+	while ($true) {
+		$Date = Get-Date
+		$Action = "Test 目標"
+		
+		if ($IsPowerShell7) {
+			# 是 PowerShell 7
+			# $IsSuccess = Test-Connection -ComputerName $Url -Quiet -Count 1
 			
+			try { 
+				$PingResult = Test-Connection -ComputerName $Url -ErrorAction Stop -Count 1
+				$ResponseTime = $PingResult.Latency
+			} catch {
+				# 測試失敗 continue
+				$Result = "失敗"
+				$Title = $Action + " " + $Result
+				Write-Color -Text "$Date => ", $Action, " $Url", $Result -Color White, White, White, Red
+				$Message = "測試時間：$Date`n"
+				$Message += "測試目標：$Url`n"
+				Send-Notification -Title $Title -Text $Date -IconType Error
+				Start-Sleep -Seconds $Intervals
+				continue
+			}
+		} else {
+			# 不是 PowerShell 7
 		}
 		
-		$date = Get-Date
-		$Message = "回應時間: $($responseTime)ms"
-		Write-Host $Message
+		$Result = "成功"
+		$Title = $Action + " " + $Result
+		# 回應時間顏色
+		$ResponseTimeColor = $ResponseTime -gt $ResponseTimeLimit ? "Red" : "Green"
+		# 格式化回應時間左邊補到 3 位
+		$ResponseTimeFormat = "{0,3}" -f $ResponseTime
+		Write-Color -Text "$Date => $Action ", $Url, " $Result", " => 回應時間: ", $ResponseTimeFormat, " ms" -Color , Yellow, Green, , $ResponseTimeColor,
 		
-		if ($responseTime -gt $Time) {
-			$Title = "Ping [$($Url)] 回應超時"
-			$Message = "$date`n$Message"
+		if ($ResponseTime -gt $ResponseTimeLimit) {
+			$Title = "Test 回應時間超過設定閾值：$ResponseTimeLimit ms"
+			$Message = "測試時間：$Date`n"
+			$Message += "測試目標：$Url`n"
+			$Message += "回應時間：$($ResponseTime) ms"
 			Send-Notification -Title $Title -Text $Message
 		}
-		Start-Sleep -Seconds 1
+		
+		Start-Sleep -Seconds $Intervals
 	}
 }
 
